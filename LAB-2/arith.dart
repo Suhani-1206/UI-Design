6a23ab45dc7dbba2999743955ae48b23ab01f51e
Suhani-1206 <suhanigaddam06@gmail.com>
2025-03-19 22:42:55 +0530

    arith.dart

--- LAB-2/arith.dart
+++ LAB-2/arith.dart
@@ -1,1 +1,33 @@
+import 'dart:io';
+void main()
+{
+	print("enter value of a:");
+	int a=int.parse(stdin.readLineSync()!);
 
+	print("enter value of b:");
+	int b=int.parse(stdin.readLineSync()!);
+	
+	stdout.writeln('Value of a is: $a');
+	stdout.writeln('Value of a is: $b');
+	sum(a,b);
+	mul(a,b);
+	sub(a,b);
+	div(a,b);
+}
+	
+void sum(a,b)
+{
+	print('sum is : ${a+b}');
+}
+void mul(a,b)
+{
+	print(a*b);
+}
+void sub(a,b)
+{
+	print(a-b);
+}
+void div(a,b)
+{
+	print(a/b);
+}
